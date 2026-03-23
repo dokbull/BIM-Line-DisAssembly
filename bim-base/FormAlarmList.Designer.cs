f31dbea1dc3c82472600c8825c092308b1470d86
gw.kim <rldns20704@naver.com>
2026-03-23 16:59:53 +0700

    [Feat] Add FormAlarmList
    알람리스트 UI 추가 (Current Alarm 클릭 시 Light알람이 상단으로 나오도록 수정 예정)

---
+++ bim-base/FormAlarmList.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace bim_base
+{
+    partial class FormAlarmList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lightAlarmButton = new System.Windows.Forms.Button();
+            this.heavyAlarmButton = new System.Windows.Forms.Button();
+            this.currentAlarmButton = new System.Windows.Forms.Button();
+            this.alarmResetButton = new System.Windows.Forms.Button();
+            this.alarmListGrid = new CSourceGrid();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lightAlarmButton);
+            this.panel1.Controls.Add(this.heavyAlarmButton);
+            this.panel1.Controls.Add(this.currentAlarmButton);
+            this.panel1.Controls.Add(this.alarmResetButton);
+            this.panel1.Location = new System.Drawing.Point(787, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(214, 602);
+            this.panel1.TabIndex = 5;
+            // 
+            // lightAlarmButton
+            // 
+            this.lightAlarmButton.Location = new System.Drawing.Point(10, 185);
+            this.lightAlarmButton.Margin = new System.Windows.Forms.Padding(5, 10, 10, 5);
+            this.lightAlarmButton.Name = "lightAlarmButton";
+            this.lightAlarmButton.Size = new System.Drawing.Size(194, 64);
+            this.lightAlarmButton.TabIndex = 6;
+            this.lightAlarmButton.Text = "LightAlarm";
+            this.lightAlarmButton.UseVisualStyleBackColor = true;
+            this.lightAlarmButton.Click += new System.EventHandler(this.lightAlarmButton_Click);
+            // 
+            // heavyAlarmButton
+            // 
+            this.heavyAlarmButton.Location = new System.Drawing.Point(10, 106);
+            this.heavyAlarmButton.Margin = new System.Windows.Forms.Padding(5, 10, 10, 5);
+            this.heavyAlarmButton.Name = "heavyAlarmButton";
+            this.heavyAlarmButton.Size = new System.Drawing.Size(194, 64);
+            this.heavyAlarmButton.TabIndex = 5;
+            this.heavyAlarmButton.Text = "HeavyAlarm";
+            this.heavyAlarmButton.UseVisualStyleBackColor = true;
+            this.heavyAlarmButton.Click += new System.EventHandler(this.heavyAlarmButton_Click);
+            // 
+            // currentAlarmButton
+            // 
+            this.currentAlarmButton.Location = new System.Drawing.Point(10, 27);
+            this.currentAlarmButton.Margin = new System.Windows.Forms.Padding(5, 10, 10, 5);
+            this.currentAlarmButton.Name = "currentAlarmButton";
+            this.currentAlarmButton.Size = new System.Drawing.Size(194, 64);
+            this.currentAlarmButton.TabIndex = 4;
+            this.currentAlarmButton.Text = "CurrentAlarm";
+            this.currentAlarmButton.UseVisualStyleBackColor = true;
+            this.currentAlarmButton.Click += new System.EventHandler(this.currentAlarmButton_Click);
+            // 
+            // alarmResetButton
+            // 
+            this.alarmResetButton.Location = new System.Drawing.Point(10, 502);
+            this.alarmResetButton.Margin = new System.Windows.Forms.Padding(5, 10, 10, 5);
+            this.alarmResetButton.Name = "alarmResetButton";
+            this.alarmResetButton.Size = new System.Drawing.Size(194, 64);
+            this.alarmResetButton.TabIndex = 3;
+            this.alarmResetButton.Text = "AlarmReset";
+            this.alarmResetButton.UseVisualStyleBackColor = true;
+            this.alarmResetButton.Click += new System.EventHandler(this.alarmResetButton_Click);
+            // 
+            // alarmListGrid
+            // 
+            this.alarmListGrid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.alarmListGrid.EnableSort = true;
+            this.alarmListGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alarmListGrid.Location = new System.Drawing.Point(4, 4);
+            this.alarmListGrid.Margin = new System.Windows.Forms.Padding(4);
+            this.alarmListGrid.Name = "alarmListGrid";
+            this.alarmListGrid.OptimizeMode = SourceGrid.CellOptimizeMode.ForRows;
+            this.alarmListGrid.SelectionMode = SourceGrid.GridSelectionMode.Cell;
+            this.alarmListGrid.Size = new System.Drawing.Size(776, 600);
+            this.alarmListGrid.TabIndex = 4;
+            this.alarmListGrid.TabStop = true;
+            this.alarmListGrid.ToolTipText = "";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 220F));
+            this.tableLayoutPanel1.Controls.Add(this.alarmListGrid, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 10);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1004, 608);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // FormAlarmList
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1024, 628);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormAlarmList";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "FormAlarmList";
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button lightAlarmButton;
+        private System.Windows.Forms.Button heavyAlarmButton;
+        private System.Windows.Forms.Button currentAlarmButton;
+        private System.Windows.Forms.Button alarmResetButton;
+        private CSourceGrid alarmListGrid;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ bim-base/FormAlarmList.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace bim_base
+{
+    public partial class FormAlarmList : Form
+    {
+        ProcessMain main = null;
+        CFileManager m_fileManager = null;
+        Queue<string[]> m_logQueue = new Queue<string[]>();
+        List<AlarmData> lightAlarm = null;
+        List<AlarmData> heavyAlarm = null;
+
+        public FormAlarmList(ProcessMain procMain)
+        {
+            InitializeComponent();
+
+            main = procMain;
+            lightAlarm = new List<AlarmData>();
+            heavyAlarm = new List<AlarmData>();
+
+            Init();
+        }
+
+        // 초기화
+        private void Init()
+        {
+            alarmListGrid.setRowCol(1, 6);
+            string[] header = new string[] { "No", "Occurrence Time", "I/O", "Code", "Level", "Message" };
+            alarmListGrid.setHeader(header);
+
+            alarmListGrid.setHeaderColor(Color.LightSkyBlue, Color.White);
+            alarmListGrid.FixedRows = 1;
+
+            int totalWidth = alarmListGrid.Width - 2;
+
+            alarmListGrid.Columns[0].Width = 50; totalWidth -= 50;
+            alarmListGrid.Columns[1].Width = 150; totalWidth -= 150;
+            alarmListGrid.Columns[2].Width = 50; totalWidth -= 50;
+            alarmListGrid.Columns[3].Width = 50; totalWidth -= 50;
+            alarmListGrid.Columns[4].Width = 70; totalWidth -= 70;
+            alarmListGrid.Columns[5].Width = totalWidth; 
+
+            alarmListGrid.setTextAlignment(0, 0, DevAge.Drawing.ContentAlignment.MiddleCenter);
+            alarmListGrid.setTextAlignment(0, 1, DevAge.Drawing.ContentAlignment.MiddleCenter);
+            alarmListGrid.setTextAlignment(0, 2, DevAge.Drawing.ContentAlignment.MiddleCenter);
+            alarmListGrid.setTextAlignment(0, 3, DevAge.Drawing.ContentAlignment.MiddleCenter);
+            alarmListGrid.setTextAlignment(0, 4, DevAge.Drawing.ContentAlignment.MiddleCenter);
+            alarmListGrid.setTextAlignment(0, 5, DevAge.Drawing.ContentAlignment.MiddleLeft);
+
+            dateChange();
+        }
+
+        private void calendar_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            dateChange();
+        }
+
+        private void dateChange()
+        {
+            DateTime date = DateTime.Now;
+
+            gridDataClear();
+
+            loadLogData(date);
+        }
+
+        private void loadLogData(DateTime datetime)
+        {
+            if (m_fileManager != null)
+            {
+                m_fileManager = null;
+            }
+
+            m_fileManager = new CFileManager(Common.LOG_PATH + "\\alarm\\" + datetime.ToString("yyyyMMdd") + ".log");
+
+            if (lightAlarm == null || lightAlarm.Count == 0)
+            {
+                return;
+            }
+
+            for (int i = 0; i < lightAlarm.Count; i++)
+            {
+                int num = (int)lightAlarm[0].alarm;
+                string date = lightAlarm[1].datetime;
+                int code = lightAlarm[3].code;
+                ALARM_TYPE type = lightAlarm[4].type;
+                string text = lightAlarm[5].desc;
+
+                addLog(num, date, code, type, text, false);
+            }
+        }
+
+        public void addLog(int num, string date, int code, ALARM_TYPE type, string text, bool useQueue = true)
+        {
+            string[] texts = new string[3];
+
+            texts[0] = num.ToString();
+            texts[1] = date;
+            texts[3] = code.ToString("0000");
+            texts[5] = text;
+
+            if (useQueue)
+                m_logQueue.Enqueue(texts);
+            else
+                addGridRowData(texts);
+        }
+
+        private void addGridRowData(string[] texts)
+        {
+            int row = 1;
+            int colCount = alarmListGrid.ColumnsCount;
+            alarmListGrid.Rows.Insert(row);
+
+            for (int i = 0; i < colCount; i++)
+            {
+                CCell cell = new CCell();
+                CViewCell viewCell = new CViewCell();
+
+                alarmListGrid[row, i] = cell;
+                alarmListGrid[row, i].View = viewCell;
+            }
+
+            alarmListGrid[row, 0].Value = texts[0];
+            alarmListGrid[row, 1].Value = texts[1];
+            alarmListGrid[row, 2].Value = texts[2];
+
+            alarmListGrid.setTextAlignment(row, 0, DevAge.Drawing.ContentAlignment.MiddleCenter);
+            alarmListGrid.setTextAlignment(row, 1, DevAge.Drawing.ContentAlignment.MiddleCenter);
+
+            int code = Util.toInt32(texts[1]);
+        }
+
+        private void gridDataClear()
+        {
+            int rowCount = alarmListGrid.RowsCount;
+            int colCount = alarmListGrid.ColumnsCount;
+
+            for (int i = rowCount - 1; i > 0; i--)
+            {
+                for (int j = 0; j < colCount; j++)
+                    alarmListGrid[i, j].Value = null;
+
+                alarmListGrid.RowsCount -= 1;
+            }
+        }
+
+        // Current Alarm Button Click
+        private void currentAlarmButton_Click(object sender, EventArgs e)
+        {
+            gridDataClear();
+
+            // 오늘 날짜의 알람 로그 파일 읽기
+            DateTime today = DateTime.Now;
+            string logPath = Common.LOG_PATH + "\\alarm\\" + today.ToString("yyyyMMdd") + ".log";
+            CFileManager fileMgr = new CFileManager(logPath);
+
+            List<string[]> lines = fileMgr.readAll(',');
+            if (lines == null || lines.Count == 0)
+                return;
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string[] split = lines[i];
+                if (split.Length < 5)
+                    continue;
+
+                int row = alarmListGrid.RowsCount;
+                int colCount = alarmListGrid.ColumnsCount;
+                alarmListGrid.addRow(1);
+
+                if (split[3].Trim() == "LIGHT")
+                {
+                    // No
+                    alarmListGrid.setValue(row, 0, (i + 1).ToString());
+                    // Occurrence Time
+                    alarmListGrid.setValue(row, 1, split[0] + " " + split[1]);
+                    // I/O
+                    alarmListGrid.setValue(row, 2, split[2].Trim());
+                    // Code
+                    alarmListGrid.setValue(row, 3, split[2].Trim());
+                    // Level
+                    alarmListGrid.setValue(row, 4, split[3].Trim());
+                    // Message
+                    alarmListGrid.setValue(row, 5, split[4].Trim());
+
+                    alarmListGrid.setTextAlignment(row, 0, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 1, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 2, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 3, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 4, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 5, DevAge.Drawing.ContentAlignment.MiddleLeft);
+                }
+
+                else if (split[3].Trim() == "HEAVY")
+                {
+                    // No
+                    alarmListGrid.setValue(row, 0, (i + 1).ToString());
+                    // Occurrence Time
+                    alarmListGrid.setValue(row, 1, split[0] + " " + split[1]);
+                    // I/O
+                    alarmListGrid.setValue(row, 2, split[2].Trim());
+                    // Code
+                    alarmListGrid.setValue(row, 3, split[2].Trim());
+                    // Level
+                    alarmListGrid.setValue(row, 4, split[3].Trim());
+                    // Message
+                    alarmListGrid.setValue(row, 5, split[4].Trim());
+
+                    alarmListGrid.setTextAlignment(row, 0, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 1, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 2, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 3, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 4, DevAge.Drawing.ContentAlignment.MiddleCenter);
+                    alarmListGrid.setTextAlignment(row, 5, DevAge.Drawing.ContentAlignment.MiddleLeft);
+                }
+
+                // Message 열 너비를 최대 desc 길이에 맞춤
+                autoFitMessageColumn(lines);
+            }
+        }
+
+        // Message 열 너비를 최대 desc 길이에 맞춤
+        private void autoFitMessageColumn(List<string[]> lines)
+        {
+            int maxWidth = 300;
+
+            using (Graphics g = alarmListGrid.CreateGraphics())
+            {
+                for (int i = 0; i < lines.Count; i++)
+                {
+                    if (lines[i].Length < 5) continue;
+
+                    string desc = lines[i][4].Trim();
+                    int textWidth = (int)g.MeasureString(desc, alarmListGrid.Font).Width + 20;
+
+                    if (textWidth > maxWidth)
+                        maxWidth = textWidth;
+                }
+            }
+
+            alarmListGrid.Columns[5].Width = maxWidth;
+        }
+
+        // Heavy Alarm Button Click
+        private void heavyAlarmButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Light Alarm Button Click
+        private void lightAlarmButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Alarm Reset Button Click
+        private void alarmResetButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
